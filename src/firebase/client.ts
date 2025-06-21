2f95458c33c2787bef2410f733579a343df7aefa
K4zura <johannbb0520@gmail.com>
2025-06-21 14:49:28 -0500

    Update client.ts

--- src/firebase/client.ts
+++ src/firebase/client.ts
@@ -1,12 +1,12 @@
 import { initializeApp } from 'firebase/app'
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyBUX4jYBWxC9Zd4rdQXD_lWQNylm0kzEuM',
-  authDomain: 'finance-dashboard-k4.firebaseapp.com',
-  projectId: 'finance-dashboard-k4',
-  storageBucket: 'finance-dashboard-k4.firebasestorage.app',
-  messagingSenderId: '319019083038',
-  appId: '1:319019083038:web:cf03389150bfb55166150f',
+  apiKey: '',
+  authDomain: '',
+  projectId: '',
+  storageBucket: '',
+  messagingSenderId: '',
+  appId: '',
 }
 
 export const app = initializeApp(firebaseConfig)
